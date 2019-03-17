f06cbbb953cf4406a50548583d0b0db32778dfb3
AlucardxTepes <alucard.557@gmail.com>
2019-03-17 16:48:27 -0400

    add tasks fragment

--- app/src/main/java/com/alucard/notes/navigation/NavigationActivity.kt
+++ app/src/main/java/com/alucard/notes/navigation/NavigationActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.alucard.notes.R
+import com.alucard.notes.TasksListFragment
 import com.alucard.notes.notes.NotesListFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.android.synthetic.main.activity_navigation.*
@@ -13,6 +14,7 @@ class NavigationActivity : AppCompatActivity() {
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
             R.id.navigation_tasks -> {
+                replaceFragment(TasksListFragment.newInstance())
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_notes -> {
@@ -26,7 +28,7 @@ class NavigationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_navigation)
-
+        replaceFragment(TasksListFragment.newInstance())
         navigationView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
 
     }
