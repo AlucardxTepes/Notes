3a105aabf34016f1fd938c7a8516db88c1301442
AlucardxTepes <alucard.557@gmail.com>
2019-03-17 15:27:20 -0400

    add notes fragment method

--- app/src/main/java/com/alucard/notes/navigation/NavigationActivity.kt
+++ app/src/main/java/com/alucard/notes/navigation/NavigationActivity.kt
@@ -2,7 +2,9 @@ package com.alucard.notes.navigation
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import com.alucard.notes.R
+import com.alucard.notes.notes.NotesListFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.android.synthetic.main.activity_navigation.*
 
@@ -11,11 +13,10 @@ class NavigationActivity : AppCompatActivity() {
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
             R.id.navigation_tasks -> {
-                messageTextView.setText(R.string.title_task)
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_notes -> {
-                messageTextView.setText(R.string.title_note)
+                replaceFragment(NotesListFragment.newInstance())
                 return@OnNavigationItemSelectedListener true
             }
         }
@@ -26,9 +27,15 @@ class NavigationActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_navigation)
 
-        messageTextView.text = getString(R.string.title_task)
         navigationView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
 
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.fragmentHolder, fragment)
+            .commit()
     }
 
 }
