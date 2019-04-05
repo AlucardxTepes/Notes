5a9026e244ce3f01ae92065928887880b0d8c5f9
Alucard <alucard.557@gmail.com>
2019-04-05 17:12:29 -0400

    add note and task creation fragments

--- app/src/main/java/com/alucard/notes/create/CreateActivity.kt
+++ app/src/main/java/com/alucard/notes/create/CreateActivity.kt
@@ -1,17 +1,54 @@
 package com.alucard.notes.create
 
+import android.net.Uri
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import android.widget.Toast.LENGTH_SHORT
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import com.alucard.notes.R
 import com.alucard.notes.navigation.NavigationActivity
-import kotlinx.android.synthetic.main.activity_create.*
 
-class CreateActivity : AppCompatActivity() {
+class CreateActivity : AppCompatActivity(), CreateNoteFragment.OnFragmentInteractionListener, CreateTaskFragment.OnFragmentInteractionListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create)
 
-        textView.text = intent.getStringExtra(NavigationActivity.FRAGMENT_TYPE_KEY)
+        supportActionBar?.title = ""
+
+        intent?.getStringExtra(NavigationActivity.FRAGMENT_TYPE_KEY).run {
+            if (this == NavigationActivity.FRAGMENT_VALUE_TASK) {
+                createFragment(CreateTaskFragment.newInstance())
+            } else if (this == NavigationActivity.FRAGMENT_VALUE_NOTE) {
+                createFragment(CreateNoteFragment.newInstance())
+            }
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_save, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+
+        when(item?.itemId) {
+            R.id.saveItem -> Toast.makeText(this, "Save clicked", LENGTH_SHORT).show()
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onFragmentInteraction(uri: Uri) {
+        // TODO
+    }
+
+    private fun createFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragmentHolder, fragment)
+            .commit()
     }
 }
